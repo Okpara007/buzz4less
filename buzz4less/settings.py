98edd5c4e3513ddc66a0d8a5f68f93ad998ed1f4
Okpara007 <chinemeremokpara93@gmail.com>
2024-09-07 11:41:41 +0100

    added to allowed hosts

--- buzz4less/settings.py
+++ buzz4less/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "true"
 
-ALLOWED_HOSTS = ['buzz4less.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['buzz4less.onrender.com', 'localhost', '127.0.0.1', 'www.buzzforless.com']
 
 # Application definition
 
