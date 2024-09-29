0afa269eca02cccad789c62302cee6c06234ae58
Okpara007 <chinemeremokpara93@gmail.com>
2024-09-29 23:22:22 +0100

    edited email code

--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Contact
-from django.core.mail import send_mail
+from django.core.mail import get_connection, send_mail
 
 # Create your views here.
 def contact(request):
@@ -15,6 +15,7 @@ def contact(request):
         # Determine if the user is authenticated
         user_id = request.user.id if request.user.is_authenticated else None
 
+        # Save the new contact
         new_contact = Contact(
             name=name, 
             email=email, 
@@ -26,7 +27,7 @@ def contact(request):
 
         # Prepare and send the email notification
         email_subject = f'A contact has been made.'
-        admin_url = 'https://buzzforless.com/admin/'  # Local admin URL
+        admin_url = 'https://buzzforless.com/admin/'  # Admin URL for more info
         email_body = (
             f'{name}.\n'
             f'There has been a contact. Sign into the admin panel for more info.\n'
@@ -34,12 +35,24 @@ def contact(request):
             f'{new_contact.message}'
         )
 
+        # Set up a custom connection for this email
+        connection = get_connection(
+            backend='django.core.mail.backends.smtp.EmailBackend',
+            host='mail.privateemail.com',  # Namecheap SMTP server
+            port=587,
+            username='support@buzzforless.com',
+            password='3f(*W37uMjVaxJP',
+            use_tls=True,
+        )
+
+        # Send the email using the custom connection
         send_mail(
             email_subject,
             email_body,
-            'support@buzzforless.com',
-            ['chinemeremokpara93@gmail.com', 'Okaforambrose2020@gmail.com'],
-            fail_silently=False
+            'support@buzzforless.com',  # Alias as the sender
+            ['chinemeremokpara93@gmail.com', 'Okaforambrose2020@gmail.com'],  # Recipients
+            connection=connection,
+            fail_silently=False,
         )
 
     return render(request, 'contacts/contacts.html')
